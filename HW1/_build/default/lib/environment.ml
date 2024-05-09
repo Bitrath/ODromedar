52bea0e41a6ba85272743c8536db2c5f8bf3bab7
nicolacava01 <nicola.cavaletti@edu.unife.it>
2024-05-09 14:54:50 +0200

    commenti

--- HW1/_build/default/lib/environment.ml
+++ HW1/_build/default/lib/environment.ml
@@ -6,20 +6,50 @@ open Ast
 let emptyenv = []
 
 (* Non-Empty Environment *)
-type 'a env = (ide * 'a) list
+  (* The environment maps variables to
+    pairs consisting of a value and taint status *)
+type 'a env = (ide * 'a * bool)  list
 
 (* Types handled from the environment *)
 type evT = Int of int
-        | Float of float
-        | Bool of bool
-        | Closure of ide * exp * evT env
-        | Unbound
+    | Float of float
+    | Bool of bool
+    | String of string
+    | Closure of ide * exp * evT env
+    | Unbound
 
 (* lookup: exhaustive search of an element into the environment *)
 let rec lookup env x =
   match env with
-    | [] -> failwith("Not Found")
-    | (y, v)::r -> if x = y then v else lookup r x
+    | [] -> failwith "Not Found"
+    | (y, v, _)::r -> if x = y then v else lookup r x
+
+
+    (*Vede se c'è un trusted block già definito con quel nome*)
+let rec clean_lookup env x =
+  match env with
+    | [] -> Int 0
+    | (y, _, _)::r -> if x = y then Int 1 else clean_lookup r x
+
+(*taintnes check, if x y binding it returns the taint status*)
+let rec t_lookup env x =
+  match env with
+    | [] -> failwith "Not Found"
+    | (y, _, t)::r -> if y = x then t else t_lookup r x
 
 (* bind: pushes a new tuple (string, element) into the environment *)
-let bind env (x:string) (v:'a) = (x,v)::env
+let bind env (x: string) (v: 'a) = (x, v)::env
+
+
+(*let rec block_lookup body blockEnv t eval = 
+  match body with 
+    | [] -> blockEnv,t
+    | exp1::tail ->  match exp1 with
+                | Let(x, eRhs, letbody ) ->   let xVal, t1 = eval eRhs blockEnv t Private in 
+                                                let letEnv = (x, xVal, t1)::blockEnv in
+                                                  let funVal, t2 = eval letbody letEnv t   Private  in
+                                                    
+                                                    block_lookup tail blockEnv t eval
+
+
+                | _ -> failwith "NOTHING ELSE, JUST Let"  *)
--- HW1/_build/default/lib/interpreter.ml
+++ HW1/_build/default/lib/interpreter.ml
@@ -1,45 +1,83 @@
 open Ast
 open Environment
-open Utils
+(* open Utils *)
 
 (* --- INTERPRETER  --- *)
 
-let rec eval(e:exp) (env: evT env ) =
+let rec eval (e: exp) (env: evT env) (t : bool) (*(sec_lev : trust)*) : evT * bool = 
   match e with
-  | CstInt(n) -> Int(n)
-  | CstFlt(n) -> Float(n)
-  | CstTrue -> Bool(true)
-  | CstFalse -> Bool(false)
-  | IsZeroFloat(e1) -> is_zero_float(eval e1 env)
-  | IsZeroInt(e1) -> is_zero_int(eval e1 env)
-  | Times(e1, e2) -> int_mul((eval e1 env), (eval e2 env))
-  | TimesF(e1, e2) -> float_mul((eval e1 env), (eval e2 env))
-  | Sum(e1, e2) -> int_plus((eval e1 env), (eval e2 env))
-  | SumF(e1, e2) -> float_plus((eval e1 env), (eval e2 env))
-  | Sub(e1, e2) -> int_minus((eval e1 env), (eval e2 env))
-  | SubF(e1, e2) -> float_minus((eval e1 env), (eval e2 env))
-  | Div(e1, e2) -> int_div((eval e1 env), (eval e2 env))
-  | DivF(e1, e2) -> float_div((eval e1 env), (eval e2 env))
-  | Eq(e1, e2) -> int_eq((eval e1 env), (eval e2 env))
-  | EqF(e1, e2) -> float_eq((eval e1 env), (eval e2 env))
-  | And(e1, e2) -> bool_and((eval e1 env), (eval e2 env))
-  | Or(e1, e2) -> bool_or((eval e1 env), (eval e2 env))
-  | Not(e1) -> bool_not(eval e1 env)
-  | Den(e1) -> lookup env e1
-  | Ifthenelse(cond, e2, e3) -> 
-          (let g = eval cond env in
-           match (typecheck("bool", g), g) with
-            | (true, Bool(true)) -> eval e2 env
-            | (true, Bool(false)) -> eval e3 env
-            | (_, _) -> failwith("Non-boolean guard"))
-  | Let(i, e, ebody) -> eval ebody(bind env i (eval e env))
-  | Fun(i, a) -> Closure(i, a, env)
-  | Apply(Den(f), eArg) ->
-    (let fclosure = lookup env f in
-      match fclosure with
-        | Closure(arg, fbody, fDecEnv) ->
-          let aVal = eval eArg env in
-          let aenv = bind fDecEnv arg aVal in
-            eval fbody aenv
-        | _ -> failwith("Non functional value"))
-  | Apply(_, _) -> failwith("Application: not first order function")
+  | CstInt n -> (Int n, t)
+  | CstFlt n -> (Float n, t)
+  | CstBool b -> (Int (if b then 1 else 0), t)
+  | Den id -> (lookup env id, t_lookup env id)
+  (*IL BODY È UN LISTA DI ESPRESSIONI*)
+(*
+  | LetSec (x, eRhs, letBody) ->(
+      
+      let xVal, t1 = eval eRhs env t in (* xVal: evT * bool != xVal, t1: evT, bool *)
+          match t1 with
+
+                          (* CONTROLLO SE IL VALORE È UNTAINED PRIMA DI AGGIUNGERLO ALL'ENVIRONMENT *)
+            | false -> let letEnv = (x, xVal, t1)::env in
+                      eval letBody letEnv t1
+            | true -> failwith "Cannot add tainted data to TrustedBlock"
+    
+  )
+  *)
+
+  | Let (x, eRhs, letBody) -> 
+     let xVal, t1 = eval eRhs env t in (* xVal: evT * bool != xVal, t1: evT, bool *)
+      let letEnv = (x, xVal, t1)::env in
+        eval letBody letEnv t1  
+  | Prim (ope, e1, e2) -> (
+      let v1, t1 = eval e1 env t in
+        let v2, t2 = eval e2 env t in
+          match (ope, v1, v2) with
+            | "*", Int i1, Int i2 -> (Int (i1 * i2), t1 || t2)
+            | "+", Int i1, Int i2 -> (Int (i1 + i2), t1 || t2)
+            | "-", Int i1, Int i2 -> (Int (i1 - i2), t1 || t2)
+            | "=", Int i1, Int i2 -> (Int (if i1 = i2 then 1 else 0), t1 || t2)
+            | "<", Int i1, Int i2 -> (Int (if i1 < i2 then 1 else 0), t1 || t2)
+            | ">", Int i1, Int i2 -> (Int (if i1 > i2 then 1 else 0), t1 || t2)
+            | "&", Bool b1, Bool b2 -> (Bool (b1 && b2), t1 || t2)
+            | "|", Bool b1, Bool b2 -> (Bool (b1 || b2), t1 || t2)
+            (*ops for foat*)
+            | "*", Float i1, Float i2 -> (Float (i1 *. i2), t1 || t2)
+            | "+", Float i1, Float i2 -> (Float (i1 +. i2), t1 || t2)
+            | "-", Float i1, Float i2 -> (Float (i1 -. i2), t1 || t2)
+            | "=", Float i1, Float i2 -> (Int (if i1 = i2 then 1 else 0), t1 || t2)
+            | "<", Float i1, Float i2 -> (Int (if i1 < i2 then 1 else 0), t1 || t2)
+            | ">", Float i1, Float i2 -> (Int (if i1 > i2 then 1 else 0), t1 || t2)
+            | _ -> failwith "unknown primitive or wrong type"  
+    )
+  | If (cond, e2, e3) -> (
+      let v1, t1 = eval cond env t in
+        match v1 with
+          | Bool true -> let v2, t2 = eval e2 env  t1  in (v2, t1 || t2)
+          | Bool false -> let v3, t3 = eval e3 env  t1  in (v3, t1 || t3)
+          | _ -> failwith "eval if" 
+    )
+  | Fun (f_param, f_body) -> (Closure (f_param, f_body, env), t)
+(* --- [ >:( ] --- *)
+  | Call (f, param) -> (
+      let fClosure, f_t = eval f env t in
+        match fClosure, f_t  with (*CLOSURE: la funzione viene valutata solo se il valore di f_t è UNTAINED ,cioè FALSE*) 
+          | Closure (f_param, f_body, fDeclEnv),  false  -> (* xVal is evaluated in the current stack *)
+              let xVal, t1 = eval param env  t  in
+                let fBodyEnv = (f_param, xVal, t1)::fDeclEnv in
+                  let f_res, t_res = eval f_body fBodyEnv  t  in 
+                    (f_res, t_res) (* NON HO PIU BISOGNO DI OR TRA I TAINTED VALUES *)
+          | _ -> failwith "eval Call: not a function"
+    )
+  | GetInput e -> eval e env true 
+  | Abort msg -> failwith msg
+  | TrustedBlock(id, body) -> 
+    (* We search for the Block Identifier *)
+    (* If available, exit the Block, it already exists!!! *)
+    (* -> Not available: Then insert it into the current env and continue with the evaluation of the body *)
+    let resultId = clean_lookup env id in 
+      match resultId, t with
+                                    (*I TRUSTED BLOCK SONO SEMPRE UNTAINTED -> FALSE*)           
+        | (_ , _) -> failwith "NO"
+  
+  | _ -> failwith "Pattern not matched"
