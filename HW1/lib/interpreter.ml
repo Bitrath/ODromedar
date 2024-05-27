8746bc43285749ca6d2349554f2f63cd3e164023
Bitrath <bitrath@users.noreply.github.com>
2024-05-27 14:42:48 +0200

    NEW Tests. Prim modified. More CLEAN Code. TO DO: Look at Let, Look at FUN/CALL.

--- HW1/lib/interpreter.ml
+++ HW1/lib/interpreter.ml
@@ -8,13 +8,15 @@ let rec eval (e: exp) (env: evT env) (taint: bool) (sec_lev: trust) : evT * bool
   match e with
   | CstInt n -> (Int n, taint)            
   | CstFlt n -> (Float n, taint)
-  | CstBool b -> (Int (if b then 1 else 0), taint)
+  (* | CstBool b -> (Int (if b then 1 else 0), taint) *)
+  | CstBool b -> (Bool b, taint)
   | CstStr s -> (String s, taint)
   | Den id -> (lookup env id, taint_lookup env id)
   | Let (x, conf, eRhs, letBody) -> ( 
       match sec_lev, conf with
         | Untrusted, Private -> failwith "LET Error: Can't make a private declaration in an Untrusted enviroment"    
         | Trusted, Public ->failwith "LET Error: Can't make a public declaration in a Trusted enviroment "
+        | BlockLvl, Public ->failwith "LET Error: Can't make a public declaration in a Trusted Block "
         | _ -> let xVal, t1 = eval eRhs env taint sec_lev in
             let letEnv = (x, xVal, t1)::env in
               eval letBody letEnv t1 sec_lev  
@@ -26,19 +28,18 @@ let rec eval (e: exp) (env: evT env) (taint: bool) (sec_lev: trust) : evT * bool
             | "*", Int i1, Int i2 -> (Int (i1 * i2), t1 || t2)
             | "+", Int i1, Int i2 -> (Int (i1 + i2), t1 || t2)
             | "-", Int i1, Int i2 -> (Int (i1 - i2), t1 || t2)
-            | "=", Int i1, Int i2 -> (Int (if i1 = i2 then 1 else 0), t1 || t2)
-            | "<", Int i1, Int i2 -> (Int (if i1 < i2 then 1 else 0), t1 || t2)
-            | ">", Int i1, Int i2 -> (Int (if i1 > i2 then 1 else 0), t1 || t2)
-            | "&", Bool b1, Bool b2 -> (Bool (b1 && b2), t1 || t2)
-            | "|", Bool b1, Bool b2 -> (Bool (b1 || b2), t1 || t2)
-            | "cmp", String s1, String s2 -> (Bool (if (String.compare s1 s2) = 0 then true else false), t1 || t2)
-            (* ops for float *)
+            | "=", Int i1, Int i2 -> (Bool (if i1 = i2 then true else false), t1 || t2)
+            | "<", Int i1, Int i2 -> (Bool (if i1 < i2 then true else false), t1 || t2)
+            | ">", Int i1, Int i2 -> (Bool (if i1 > i2 then true else false), t1 || t2)
             | "*", Float i1, Float i2 -> (Float (i1 *. i2), t1 || t2)
             | "+", Float i1, Float i2 -> (Float (i1 +. i2), t1 || t2)
             | "-", Float i1, Float i2 -> (Float (i1 -. i2), t1 || t2)
-            | "=", Float i1, Float i2 -> (Int (if i1 = i2 then 1 else 0), t1 || t2)
-            | "<", Float i1, Float i2 -> (Int (if i1 < i2 then 1 else 0), t1 || t2)
-            | ">", Float i1, Float i2 -> (Int (if i1 > i2 then 1 else 0), t1 || t2)
+            | "=", Float i1, Float i2 -> (Bool (if i1 = i2 then true else false), t1 || t2)
+            | "<", Float i1, Float i2 -> (Bool (if i1 < i2 then true else false), t1 || t2)
+            | ">", Float i1, Float i2 -> (Bool (if i1 > i2 then true else false), t1 || t2)
+            | "&", Bool b1, Bool b2 -> (Bool (b1 && b2), t1 || t2)
+            | "|", Bool b1, Bool b2 -> (Bool (b1 || b2), t1 || t2)
+            | "cmp", String s1, String s2 -> (Bool (if (String.compare s1 s2) = 0 then true else false), t1 || t2)
             | _ -> failwith "PRIM Error: Unknown primitive or wrong type"  
     )
   | If (cond, e2, e3) -> (
@@ -49,73 +50,46 @@ let rec eval (e: exp) (env: evT env) (taint: bool) (sec_lev: trust) : evT * bool
           | _ -> failwith "eval if" 
     )
   | Fun (f_param, f_body) -> (Closure (f_param, f_body, env), taint)
-(* --- [ >:( ] --- *)
-  | Call (f, param) -> ( (* Call -> chiamata a funzione f *)
-
-      let fClosure, f_t = eval f env taint sec_lev in 
-      (* Questa riga valuta l'espressione f (che rappresenta la funzione da chiamare)
-        nell'ambiente corrente env con il flag di taintedness t.
-        Restituisce due valori: fClosure -> rappresenta la chiusura della 
-        funzione (se f è una funzione) e f_t -> rappresenta il flag di 
-        taintedness associato a f. *)
-
-        match fClosure, f_t  with (* fClosure -> funzione insieme all'ambiente in 
-                                    cui e' stata definita *)
-
-            (* f_param -> parametro formale funzione, f_body -> corpo della funzione, 
-              fDeclEnv -> ambiente in cui la funzione e' stata definita *)
-          | Closure (f_param, f_body, fDeclEnv),  false  -> 
-          (* CLOSURE: la funzione viene eseguita solo se il valore di f_t è UNTAINTED *) 
-            (* xVal is evaluated in the current stack *)
-              let xVal, t1 = eval param env taint sec_lev in (* param (che rappresenta gli argomenti 
-                della funzione) viene valutato nell'ambiente corrente env con il flag di
-                taintedness t, restituendo xVal come valore dell'argomento e t1 come flag 
-                di taintedness associato all'argomento. *)
+  | Call (f, param) -> (
+      let fClosure, f_t = eval f env taint sec_lev in
+        match fClosure, f_t with
+          | Closure (f_param, f_body, fDeclEnv),  false  ->
+              let xVal, t1 = eval param env taint sec_lev in
                 let fBodyEnv = (f_param, xVal, t1)::fDeclEnv in
-                (* costruzione di un nuovo ambiente fBodyEnv estendendo l'ambiente 
-                  (concatenazione) dichiarativo fDeclEnv con il valore dell'argomento 
-                  xVal associato al parametro formale f_param. Il flag di taintedness 
-                  associato all'argomento t1 viene mantenuto per tenere traccia della 
-                  "contaminazione" dei valori. *)
                   let f_res, t_res = eval f_body fBodyEnv taint sec_lev in 
-                    (f_res, t_res) (* NON HO PIU' BISOGNO DI OR TRA I TAINTED VALUES *)
-          | _ -> failwith "eval Call: not a function"
+                    (f_res, t_res)
+          | _ -> failwith "CALL Error: (Closure) not found."
     )
   | GetInput e -> eval e env true sec_lev
-  | Abort msg -> failwith msg
-  | TrustedBlock(id, body) -> 
-    (* We search for the Block Identifier *)
-    (* If available, exit the Block, it already exists!!! *)
-    (* -> Not available: Then insert it into the current env and continue with the evaluation of the body *)
-        if (clean_lookup env id) = Int 1 then
-            failwith ("TRUSTED BLOCK Error: (" ^ id ^ ") is already declared as a Trusted Block")
-        else if sec_lev = BlockLvl then
-            failwith ("TRUSTED BLOCK Error: can't declare a Trusted Block inside another one")
-        else if sec_lev = Untrusted then
-            failwith ("TRUSTED BLOCK Error: can't declare a Trusted Block inside of UNTRUSTED code")
-        else (
-            let newBlockEnv = [] in 
-              let bodyRes, evalTaint = eval body newBlockEnv taint BlockLvl in 
-                match bodyRes with 
-                  | ClosureTrustedBlock(_) -> ( if evalTaint = true then failwith "tainted." 
-                      else 
-                        let extEnv = (id, bodyRes, evalTaint)::env in 
-                          eval (EndTrustedBlock(id)) extEnv evalTaint sec_lev
-                    )
-                  | _ -> failwith "TRUSTED BLOCK Error: The Block ended with an expression different than an Handle. Error."
-          ) 
+  | TrustedBlock(id, body) -> (
+      if (clean_lookup env id) = Int 1 then
+          failwith ("TRUSTED BLOCK Error: (" ^ id ^ ") is already declared as a Trusted Block")
+      else if sec_lev = BlockLvl then
+          failwith ("TRUSTED BLOCK Error: Cannot declare a Trusted Block inside ANOTHER one")
+      else if sec_lev = Untrusted then
+          failwith ("TRUSTED BLOCK Error: Cannot declare a Trusted Block inside of UNTRUSTED code")
+      else (
+          let newBlockEnv = [] in 
+            let bodyRes, evalTaint = eval body newBlockEnv taint BlockLvl in 
+              match bodyRes with 
+                | ClosureTrustedBlock(_) -> ( if evalTaint = true then failwith "tainted." 
+                    else 
+                      let extEnv = (id, bodyRes, evalTaint)::env in 
+                        eval (EndTrustedBlock(id)) extEnv evalTaint sec_lev
+                  )
+                | _ -> failwith "TRUSTED BLOCK Error: The Block ended with an expression different than an (Handle). Error."
+      ) 
+    )
     | Handle (ideFun) -> (
-      (* cerchiamo il nome della handle fun *)
         if sec_lev != BlockLvl then failwith "HANDLE Error: Cannot declare a Handle Function OUTSIDE the Trusted Block"
         else
-        let res, resTaint = eval (Den(ideFun)) env taint sec_lev in (* guardiamo se l'ide della handle function è presente nell'env del block*)
+        let res, resTaint = eval (Den(ideFun)) env taint sec_lev in
           match res with 
             | Closure(_) -> if resTaint = true then failwith "HANDLE Error: Tainted. Do not proceed." 
                  else (ClosureTrustedBlock(env), resTaint)
             | _ -> failwith "HANDLE Error: no such identifier found in the Block environment."
-      ) 
+    ) 
     | EndTrustedBlock(i) -> (
-       (* estraiamo il trusted block... *)
        let res, resT = eval (Den(i)) env taint sec_lev in
         match res with 
          | ClosureTrustedBlock(envBlock) -> (
@@ -127,23 +101,23 @@ let rec eval (e: exp) (env: evT env) (taint: bool) (sec_lev: trust) : evT * bool
                   | _ -> extractHandle tail )
             ) in 
               let hName = extractHandle envBlock in 
-                 if hName = "None" then failwith "END TRUSTED BLOCK Error: (Trusted Block Handle) no such identifier found." 
+                 if hName = "None" then failwith "END-TRUSTED-BLOCK Error: (Handle) no such identifier found." 
                     else
                       let newEnv = (hName, HandleFlag(i), resT)::env in 
                         (Env(newEnv), resT)
           )
-         | _ -> failwith "END TRUSTED BLOCK Error: No ClosureTrustedBlock() in the environment. Error."
+         | _ -> failwith "END-TRUSTED-BLOCK Error: No (ClosureTrustedBlock) in the environment. Error."
     )
     | Include(pluginTrust, id, pluginCode) -> (
         match sec_lev with 
-          | BlockLvl -> failwith "INCLUDE Error: cannot include a Plug-In INSIDE of a Trusted Block"
+          | BlockLvl -> failwith "INCLUDE Error: Cannot include a Plug-In INSIDE of a Trusted Block"
           | _ -> ( let updatedEnv = (id, ClosureInclude(pluginTrust, pluginCode), taint)::env in 
               if String.length id = 0 && pluginCode = Empty then 
                 failwith "INCLUDE Error: Empty Include."
               else 
                 (Env(updatedEnv), taint) 
             )
-      )
+    )
     | EndInclude -> (ExecuteCheck env, taint)
     | Execute(exName, exBody) -> (
         let exEVT, exTaint = eval (Den(exName)) env taint sec_lev in 
@@ -210,78 +184,5 @@ let rec eval (e: exp) (env: evT env) (taint: bool) (sec_lev: trust) : evT * bool
             )
           | _ -> failwith "HANDLE CALL: (Handle Tag) not found."
         )
+    | Abort msg -> failwith msg
     | Empty -> failwith "EMPTY: Nothing to evaluate."
-    (*| _ -> failwith "Generic Error."*)
-
-(* 
-  la nostra handle e' implicitamente definita quadno chiamiamo il trustedBlock. In questo modo abbiamo risolto
-    il bisogno di definire la keyword "Handle";
-*)
-
-(* TrustedBlock (ide * expr * expr) -> Fun(...) -> Closure(ide, ...) -> ... -> Handle(ideFun) -> (..)::envPriv
-                                                                      (ideHandle, FlagHandle(ideTrustB), taint)::(ideTrustB, ClosureEnvPriv(envPriv), taint)::env
-   let trustBlockOne(:ide) = 
-      trust {
-        body(:exp) 
-      }
-
-body(:exp) =
-    let exp1 = in 
-          (...) in 
-    handle funName;; -> Handle( ide, exp )
-
-
-    ENV Principale: (ide * evT * taint) list
-                    (nomeHandle * evT: Handle * taint)
-
-    ENV 
-    
-Enclave ide * exp(tutto)* exp(include)
-*)
-
-(* IL BODY E' UN LISTA DI ESPRESSIONI *)
-(*
-  | LetSec (x, eRhs, letBody) ->(
-      
-      let xVal, t1 = eval eRhs env t in (* xVal: evT * bool != xVal, t1: evT, bool *)
-          match t1 with
-
-                          (* CONTROLLO SE IL VALORE È UNTAINED PRIMA DI AGGIUNGERLO ALL'ENVIRONMENT *)
-            | false -> let letEnv = (x, xVal, t1)::env in
-                      eval letBody letEnv t1
-            | true -> failwith "Cannot add tainted data to TrustedBlock"
-    
-  )
-  *)
-
-  (*
-
-    (..)::(ideHandle, HandleFlag(ideTrustedBlock), taintness)::env
-
-    Include(pluginTrust, pluginID, pluginCode)
-
-    
-    Execute(puginName, code)
-    | -> Calls up ClosureInclude(pluginTrust, pluginCode)
-    | -> Trusted: eval pluginCode Trusted
-    |  | -> eval code Trusted
-    | -> Untrusted: eval pluginCode Untrusted
-    |  | -> eval code Trusted
-  
-
-    #include <plugin>
-
-    struct trustedBlock {
-      int x = 1;
-      int sum(int y){
-        return x + y;
-      }
-      handle sum;
-    }
-
-    int execute(){
-      plugin.fun()
-      plugin.var = 10;
-      handle.sum(plugin.var)
-    }
-  *)
--- HW1/test/test_HW1.ml
+++ HW1/test/test_HW1.ml
@@ -2,22 +2,27 @@ open OUnit2
 open HW1.Ast
 open HW1.Environment
 open HW1.Interpreter
-(*
-let rec sum = function
-  | [] -> 0
-  | x :: xs -> x + sum xs
 
-  let tests = "test suite for sum" >::: [
-    "empty" >:: (fun _ -> assert_equal 0 (sum []));
-    "singleton" >:: (fun _ -> assert_equal 1 (sum [1]));
-    "two_elements" >:: (fun _ -> assert_equal 3 (sum [1; 2]));
+(* --- Test (lookup_env) Setup ---------------------- *)
+(* --- Test (eval_Int) Setup ------------------------ *)
+(* --- Test (eval_Prim) Setup ----------------------- *)
+(* --- Test (eval_Let) Setup ------------------------ *)
+let expLet = Let("y", Private, (CstInt 2), (Prim("+", Den("x"), Den("y"))));;
+let envLet = [("x", Int 1, false)];;
 
-    eval_test "eval_Prim" (Int 1, false) (CstInt 1) envTest false Trusted;
-  let envTest = [("x", Int 1, false), ("y", Int 2, false)]
-  *)
+(* --- Test (eval_If) Setup ------------------------- *)
+let expIF = If((Prim("=", Den("x"), Den("y"))), (Prim("-", Den("x"), Den("y"))), (Prim("+", Den("x"), Den("y"))));;
+let envIF_e1 = [("x", Int 2, false); ("y", Int 2, false)];;
+let envIF_e2 = [("x", Int 1, false); ("y", Int 2, false)];;
 
-(* --- Test Expressions --- *)
-let testTrustedBlock = (
+(* --- Test (eval_Fun) Setup ------------------------ *)
+let expFun = Fun("x1", (Prim("+", Den("x1"), (CstInt 1))));;
+let returnFun = Closure("x1", (Prim("+", Den("x1"), (CstInt 1))), [])
+let expCall = Let("sum", Public, (expFun), (Call(Den "sum", Den "x")));;
+let envCall = [("x", Int 3, false); ("y", Int 2, false)];;
+
+(* --- Test (eval_TrustedBlock1) Setup -------------- *)
+let expTrustedBlock = (
   TrustedBlock(
     "trustB1",
     (
@@ -41,12 +46,108 @@ let testTrustedBlock = (
       )
     )
   )
-)
-let expInclude_1 = Include(Untrusted, "plugin", (Let("xIn", Public, (CstInt 1), (Let ("yIn", Public, (CstInt 3), (Prim("+", Den "xIn", Den "yIn")))))))
-let expExec_mul = Execute("pluginMul", (Let("n1", Public, (CstInt 3), (Call(Den("mul"), Den("n1"))))))
-let expHandleCall_Den = (Let("n", Private, (CstInt 10), (HandleCall("sum", Den("n")))))
-let expHandleCall_CstInt = (HandleCall("sum", (CstInt 7)))
-let expExec_Psw = Execute("myFilter", (HandleCall("checkPassword", Den("ext"))))
+);;
+let envTrustedBlock = [("sum", HandleFlag "trustB1", false); 
+  ("trustB1", ClosureTrustedBlock [("sum", Closure ("y", Prim ("+", Den "x", Den "y"), [("x", Int 1, false)]), false);
+  ("x", Int 1, false)], false)];;
+  
+(* --- Test (eval_Handle) Setup --------------------- *)
+let envHandle = [("sum", Closure ("y", Prim ("+", Den "x", Den "y"), [("x", Int 1, false)]), false)];;
+
+(* --- Test (eval_Include) Setup ------------------ *)
+let expInclude = Include(Untrusted, "plugin", (Let("xIn", Public, (CstInt 1), (Let ("yIn", Public, (CstInt 3), (Prim("+", Den "xIn", Den "yIn")))))));;
+let envInclude = [("plugin", ClosureInclude(Untrusted, (Let("xIn", Public, (CstInt 1), (Let ("yIn", Public, (CstInt 3), (Prim("+", Den "xIn", Den "yIn"))))))), false)];;
+
+(* --- Test (eval_Exec_mul) Setup ------------------- *)
+let expExec_mul = Execute("pluginMul", (Let("n1", Public, (CstInt 3), (Call(Den("mul"), Den("n1"))))));;
+let envInclude_Mul = [("pluginMul", ClosureInclude(Untrusted, (Let("mul", Public, (Fun("n1", Prim("*", Den("n1"), CstInt 2))), (EndInclude)))), false)];;
+
+(* --- Test (eval_HandleCall_TB1_Den) Setup --------- *)
+let expHandleCall_Den = (Let("n", Private, (CstInt 10), (HandleCall("sum", Den("n")))));;
+
+(* --- Test (eval_HandleCall_TB1_CstInt) Setup ------ *)
+let expHandleCall_CstInt = (HandleCall("sum", (CstInt 7)));;
+
+(* --- Test (eval_HandleCall_TB2_Psw) Setup --------- *)
+let expExec_Psw = Execute("myFilter", (HandleCall("checkPassword", Den("ext"))));;
+let envExecCheckPSW = [
+  ("myFilter", ClosureInclude (Untrusted, Let ("ext", Public, CstStr "abcd", EndInclude)), false);
+  ("checkPassword", HandleFlag "trustB2", false);
+  ("trustB2", ClosureTrustedBlock[
+    ("checkPassword", Closure ("guess", Prim ("cmp", Den "password", Den "guess"), [("password", String "abcd", false)]), false);
+    ("password", String "abcd", false)
+    ], false)
+  ];;
+
+(* --- Test Functions --- *)
+let lookup_test name expected_output (env_i: evT env) (ide_i: ide) = 
+  name >:: (fun _ -> assert_equal expected_output (lookup env_i ide_i))
+
+let taint_lookup_test name expected_output (env_i: evT env) (ide_i: ide) = 
+  name >:: (fun _ -> assert_equal expected_output (taint_lookup env_i ide_i))
+let clean_lookup_test name expected_output (env_i: evT env) (ide_i: ide) = 
+  name >:: (fun _ -> assert_equal expected_output (clean_lookup env_i ide_i))
+
+let eval_test name expected_output (exp_i: exp) (env_i: evT env) (taint_i: bool) (sec_i: trust) =
+  name >:: (fun _ -> assert_equal expected_output (eval exp_i env_i taint_i sec_i) ~msg:"Failed at Eval")
+
+(* ----------------------------------------- *)
+(* -------------- (Tests) ------------------ *)
+(* ----------------------------------------- *)
+let tests = "Test Suite for Interpreter" >::: [
+  (* --- Lookup Tests -------------------- *)
+  lookup_test "lookup_x" (Float 1.) ([("x", Float 1., false); ("y", Int 2, false)]) ("x": ide);
+  taint_lookup_test "taint_lookup_x" (false) ([("x", Int 1, false); ("y", Int 2, true)]) ("x": ide);
+  clean_lookup_test "clean_lookup_x" (Int 1) ([("x", Float 1., false); ("y", Int 2, false)]) ("x": ide);
+  (* --- Eval (Type) Tests --------------- *)
+  eval_test "eval_type_int" (Int 1, false) (CstInt 1) [] false Trusted;
+  eval_test "eval_type_float" (Float 1., false) (CstFlt 1.) [] false Trusted;
+  eval_test "eval_type_bool" (Bool true, false) (CstBool true) [] false Trusted;
+  eval_test "eval_type_string" (String "Ok", false) (CstStr "Ok") [] false Trusted;
+  eval_test "eval_type_den" (Int 0, false) (Den "x") ([("x", Int 0, false); ("y", Int 2, false)]) false Trusted;
+  (* --- Eval (Let) Tests ---------------- *)
+  eval_test "eval_let" (Int 3, false) expLet envLet false Trusted;
+  (* --- Eval (Prim) Tests --------------- *)
+  eval_test "eval_prim_int_mul" (Int 2, false) (Prim("*", Den "x", Den "y")) [("x", Int 2, false); ("y", Int 1, false)] false Trusted;
+  eval_test "eval_prim_int_sum" (Int 3, false) (Prim("+", Den "x", Den "y")) [("x", Int 2, false); ("y", Int 1, false)] false Trusted;
+  eval_test "eval_prim_int_sub" (Int 1, false) (Prim("-", Den "x", Den "y")) [("x", Int 2, false); ("y", Int 1, false)] false Trusted;
+  eval_test "eval_prim_int_cmp" (Bool true, false) (Prim("=", Den "x", Den "y")) [("x", Int 1, false); ("y", Int 1, false)] false Trusted;
+  eval_test "eval_prim_int_>" (Bool true, false) (Prim(">", Den "x", Den "y")) [("x", Int 2, false); ("y", Int 1, false)] false Trusted;
+  eval_test "eval_prim_int_<" (Bool true, false) (Prim("<", Den "x", Den "y")) [("x", Int 1, false); ("y", Int 2, false)] false Trusted;
+  eval_test "eval_prim_float_mul" (Float 2., false) (Prim("*", Den "x", Den "y")) [("x", Float 2., false); ("y", Float 1., false)] false Trusted;
+  eval_test "eval_prim_float_sum" (Float 3., false) (Prim("+", Den "x", Den "y")) [("x", Float 2., false); ("y", Float 1., false)] false Trusted;
+  eval_test "eval_prim_float_sub" (Float 1., false) (Prim("-", Den "x", Den "y")) [("x", Float 2., false); ("y", Float 1., false)] false Trusted;
+  eval_test "eval_prim_float_cmp" (Bool true, false) (Prim("=", Den "x", Den "y")) [("x", Float 1., false); ("y", Float 1., false)] false Trusted;
+  eval_test "eval_prim_float_>" (Bool true, false) (Prim(">", Den "x", Den "y")) [("x", Float 2., false); ("y", Float 1., false)] false Trusted;
+  eval_test "eval_prim_float_<" (Bool true, false) (Prim("<", Den "x", Den "y")) [("x", Float 1., false); ("y", Float 2., false)] false Trusted;
+  eval_test "eval_prim_bool_&" (Bool false, false) (Prim("&", Den "x", Den "y")) [("x", Bool true, false); ("y", Bool false, false)] false Trusted;
+  eval_test "eval_prim_bool_|" (Bool true, false) (Prim("|", Den "x", Den "y")) [("x", Bool true, false); ("y", Bool false, false)] false Trusted;
+  eval_test "eval_prim_string_cmp" (Bool true, false) (Prim("cmp", Den "x", Den "y")) [("x", String "test", false); ("y", String "test", false)] false Trusted;
+  (* --- Eval (If) Tests ----------------- *)
+  eval_test "eval_if_e1" (Int 0, false) expIF envIF_e1 false Trusted;
+  eval_test "eval_if_e2" (Int 3, false) expIF envIF_e2 false Trusted;
+  (* --- Eval (Fun) Tests ---------------- *)
+  eval_test "eval_fun" (returnFun, false) expFun [] false Trusted;
+  eval_test "eval_fun_call" (Int 4, false) expCall envCall false Untrusted;
+  (* --- Eval (GetInput) Tests ----------- *)
+  (* --- Eval (Trusted Block) Tests ------ *)
+  eval_test "eval_trusted_block" (Env envTrustedBlock, false) expTrustedBlock [] false Trusted;
+  (* --- Eval (Handle) Tests ------------- *)
+  eval_test "eval_handle" (ClosureTrustedBlock envHandle, false) (Handle "sum") envHandle false BlockLvl;
+  (* --- Eval (Include) Tests ------------ *)
+  eval_test "eval_include" (Env envInclude, false) expInclude [] false Trusted;
+  (* --- Eval (Execute) Tests ------------ *)
+  eval_test "eval_exec_mul" (Int 6, false) expExec_mul envInclude_Mul false Untrusted;
+  (* --- Eval (HandleCall) Tests --------- *)
+  eval_test "eval_handleCall_den" (Int 11, false) expHandleCall_Den envTrustedBlock false Trusted;
+  eval_test "eval_handleCall_int" (Int 8, false) expHandleCall_CstInt envTrustedBlock false Trusted;
+  eval_test "eval_handleCall_psw" (Bool true, false) expExec_Psw envExecCheckPSW false Trusted;
+];;
+
+let _ = run_test_tt_main tests;;
+
+
+(* --- SETUPS UNUSED --- *)
 (*
 let expTrustedBlockPsw = (
   TrustedBlock(
@@ -72,26 +173,14 @@ let expTrustedBlockPsw = (
       )
     )
   )
-)*)
-(*
-let expIncludePsw = Include(Untrusted, "myFilter", (Let("ext", Public, (CstStr "acbd"), (EndInclude))))*)
+)
+let expIncludePsw = Include(Untrusted, "myFilter", (Let("ext", Public, (CstStr "acbd"), (EndInclude))))
 
-(* --- Test Environments --- *)
-let envTrustedBlock1 = [("sum", HandleFlag "trustB1", false); 
-  ("trustB1", ClosureTrustedBlock [("sum", Closure ("y", Prim ("+", Den "x", Den "y"), [("x", Int 1, false)]), false);
-  ("x", Int 1, false)], false)]
-(*
 let envTrustedBlock2 = [("checkPassword", HandleFlag "trustB2", false); 
   ("trustB2", ClosureTrustedBlock [("checkPassword", Closure ("guess", Prim("cmp", Den "password", Den "guess"), [("password", String "abcd", false)]), false);
-  ("password", String "abcd", false)], false)]*)
+  ("password", String "abcd", false)], false)]
 
-let envHandle_1 = [("sum", Closure ("y", Prim ("+", Den "x", Den "y"), [("x", Int 1, false)]), false)]
-(*
-let envHandle_2 = [("checkPassword", Closure ("guess", Prim ("cmp", Den "password", Den "guess"), [("password", String "abcd", false)]), false)]*)
-
-let envInclude_1 = [("plugin", ClosureInclude(Untrusted, (Let("xIn", Public, (CstInt 1), (Let ("yIn", Public, (CstInt 3), (Prim("+", Den "xIn", Den "yIn"))))))), false)]
-
-let envInclude_Mul = [("pluginMul", ClosureInclude(Untrusted, (Let("mul", Public, (Fun("n1", Prim("*", Den("n1"), CstInt 2))), (EndInclude)))), false)]
+let envHandle_2 = [("checkPassword", Closure ("guess", Prim ("cmp", Den "password", Den "guess"), [("password", String "abcd", false)]), false)]
 
 let envExecCheckPSW = [
   ("myFilter", ClosureInclude (Untrusted, Let ("ext", Public, CstStr "abcd", EndInclude)), false);
@@ -108,26 +197,5 @@ let envOne, taintOne = eval expTrustedBlockPsw [] false Trusted
 (* 2) Plugin Exp *)
 let envTwo, taintTwo = eval expIncludePsw envOne taintOne Trusted
 (* 3) Exec Plugin with Handle Call *)
+*) 
 *)
-(* --- Test Functions --- *)
-let lookup_test name expected_output (env_i: evT env) (ide_i: ide) = 
-  name >:: (fun _ -> assert_equal expected_output (lookup env_i ide_i))
-
-let eval_test name expected_output (exp_i: exp) (env_i: evT env) (taint_i: bool) (sec_i: trust) =
-  name >:: (fun _ -> assert_equal expected_output (eval exp_i env_i taint_i sec_i) ~msg:"Failed at Eval")
-
-(* --- Tests --- *)
-let tests = "Test Suite for Interpreter" >::: [
-  lookup_test "lookup_env" (Int 1) [("x", Int 1, false)] ("x": ide);
-  eval_test "eval_Int" (Int 1, false) (CstInt 1) [] false Trusted;
-  eval_test "eval_Prim" (Int 3, false) (Prim("+", Den "x", Den "y")) [("x", Int 1, false); ("y", Int 2, false)] false Trusted;
-  eval_test "eval_TrustedBlock1" (Env envTrustedBlock1, false) testTrustedBlock [] false Trusted;
-  eval_test "eval_Handle" (ClosureTrustedBlock envHandle_1, false) (Handle "sum") envHandle_1 false BlockLvl;
-  eval_test "eval_Include_1" (Env envInclude_1, false) expInclude_1 [] false Trusted;
-  eval_test "eval_Exec_mul" (Int 6, false) expExec_mul envInclude_Mul false Untrusted;
-  eval_test "eval_HandleCall_TB1_Den" (Int 11, false) expHandleCall_Den envTrustedBlock1 false Trusted;
-  eval_test "eval_HandleCall_TB1_CstInt" (Int 8, false) expHandleCall_CstInt envTrustedBlock1 false Trusted;
-  eval_test "eval_HandleCall_TB2_Psw" (Bool true, false) expExec_Psw envExecCheckPSW false Trusted;
-]
-
-let _ = run_test_tt_main tests 
